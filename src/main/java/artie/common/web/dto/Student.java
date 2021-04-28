7668cde00eb5c5dfe85b314089ff72cdcf11f58b
Luis-Eduardo Imbernon <leimbernon@ai-mind.com>
2021-04-28 19:48:01 +0200

    scratch_gui-41: Added the age field.

--- src/main/java/artie/common/web/dto/Student.java
+++ src/main/java/artie/common/web/dto/Student.java
@@ -9,6 +9,7 @@ public class Student {
     private String studentNumber;
     private int gender;
     private int motherTongue;
+    private int age;
     private int competence;
     private int motivation;
 
@@ -42,6 +43,9 @@ public class Student {
     public int getMotherTongue(){return motherTongue;}
     public void setMotherTongue(int motherTongue){this.motherTongue = motherTongue;}
 
+    public int getAge(){return age;}
+    public void setAge(int age){this.age = age;}
+
     public int getCompetence() {
         return competence;
     }
@@ -69,16 +73,18 @@ public class Student {
      * @param studentNumber
      * @param gender
      * @param motherTongue
+     * @param age
      * @param competence
      * @param motivation
      */
-    public Student(String id, String name, String lastName, String studentNumber, int gender, int motherTongue, int competence, int motivation){
+    public Student(String id, String name, String lastName, String studentNumber, int gender, int motherTongue, int age, int competence, int motivation){
     	this.id = id;
     	this.name = name;
     	this.lastName = lastName;
     	this.studentNumber = studentNumber;
     	this.gender = gender;
     	this.motherTongue = motherTongue;
+    	this.age = age;
     	this.competence = competence;
     	this.motivation = motivation;
     }
